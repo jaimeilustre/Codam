96fec3caa16f9e713fa0f543f13f490a2b53b8de
Jaime Joaquin Gonzales Ilustre <jilustre@f0r5s14.codam.nl>
2024-12-02 13:52:23 +0100

    added today's changes

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/27 15:24:06 by jilustre      #+#    #+#                 */
-/*   Updated: 2024/11/29 15:25:06 by jilustre      ########   odam.nl         */
+/*   Updated: 2024/12/02 12:18:46 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,29 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
 #include "pipex.h"
+
+void handle_error(const char *msg)
+{
+	char	*error_message;
+
+	error_message = strerror(errno);
+	write(STDERR_FILENO, msg, ft_strlen(msg));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, error_message, ft_strlen(error_message));
+	write(STDERR_FILENO, "\n", 1);
+}
 
 int	open_input_file(char *filename)
 {
 	int	fd;
 
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 	{
-		perror("Error opening input file");
-		exit(EXIT_FAILURE);
+		handle_error("Error opening input file");
+		exit(1);
 	}
 	return (fd);
 }
@@ -37,10 +49,10 @@ int	open_output_file(char *filename)
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd < 0)
+	if (fd == -1)
 	{
-		perror("Error opening output file");
-		exit(EXIT_FAILURE);
+		handle_error("Error opening output file");
+		exit(1);
 	}
 	return (fd);
 }
@@ -74,7 +86,10 @@ char	*get_command_path(char *cmd, char **envp)
 	{
 		full_path = malloc(strlen(paths[i]) + strlen(cmd) + 2);
 		if (!full_path)
-			break ;
+		{
+			free(paths);
+			return (NULL);
+		}
 		sprintf(full_path, "%s/%s", paths[i], cmd);
 		if (stat(full_path, &buffer) == 0)
 		{
@@ -97,29 +112,32 @@ void	execute_command(char *cmd, char **envp)
 	cmd_path = get_command_path(args[0], envp);
 	if (!args || !args[0])
 	{
-		perror("Error splitting command or empty command");
-		exit(EXIT_FAILURE);
+		handle_error("Error splitting command or empty command");
+		exit(1);
 	}
 	if (!cmd_path)
 	{
 		fprintf(stderr, "Command not found: %s\n", args[0]);
 		free(args);
-		exit(EXIT_FAILURE);
+		exit(127);
 	}
-	execve(cmd_path, args, envp);
-	perror("Error executing command");
+	if (execve(cmd_path, args, envp) < 0)
+	{
+		handle_error("Error executing command");
+		free(args);
+		free(cmd_path);
+		exit(1);
+	}
+	free(args);
 	free(cmd_path);
-	free (args);
-	exit(EXIT_FAILURE);
 }
-
 
 void	create_pipe(int pipe_fd[])
 {
 	if (pipe(pipe_fd) < 0)
 	{
-		perror("Error creating pipe");
-		exit(EXIT_FAILURE);
+		handle_error("Error creating pipe");
+		exit(1);
 	}
 }
 
@@ -127,8 +145,8 @@ void	first_child(int input_fd, int pipe_fd[], char *cmd, char **envp)
 {
 	if (dup2(input_fd, STDIN_FILENO) < 0 || dup2(pipe_fd[1], STDOUT_FILENO) < 0)
 	{
-		perror("Error with dup2 in first child");
-		exit(EXIT_FAILURE);
+		handle_error("Error with dup2 in first child");
+		exit(1);
 	}
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
@@ -140,8 +158,8 @@ void	second_child(int output_fd, int pipe_fd[], char *cmd, char **envp)
 {
 	if (dup2(pipe_fd[0], STDIN_FILENO) < 0 || dup2(output_fd, STDOUT_FILENO) < 0)
 	{
-		perror("Error with dup2 in second child");
-		exit(EXIT_FAILURE);
+		handle_error("Error with dup2 in second child");
+		exit(1);
 	}
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
@@ -156,6 +174,8 @@ void	pipex(char **argv, char **envp)
 	int		pipe_fd[2];
 	pid_t	pid1;
 	pid_t	pid2;
+	int		status1;
+	int		status2;
 
 	input_fd = open_input_file(argv[1]);
 	output_fd = open_output_file(argv[4]);
@@ -163,16 +183,16 @@ void	pipex(char **argv, char **envp)
 	pid1 = fork();
 	if (pid1 < 0)
 	{
-		perror("Error with fork for first child");
-		exit(EXIT_FAILURE);
+		handle_error("Error with fork for first child");
+		exit(1);
 	}
 	else if (pid1 == 0)
 		first_child(input_fd, pipe_fd, argv[2], envp);
 	pid2 = fork();
 	if (pid2 < 0)
 	{
-		perror("Error with fork for second child");
-		exit(EXIT_FAILURE);
+		handle_error("Error with fork for second child");
+		exit(1);
 	}
 	else if (pid2 == 0)
 		second_child(output_fd, pipe_fd, argv[3], envp);
@@ -180,8 +200,14 @@ void	pipex(char **argv, char **envp)
 	close(pipe_fd[1]);
 	close(input_fd);
 	close(output_fd);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	waitpid(pid1, &status1, 0);
+	waitpid(pid2, &status2, 0);
+	if (WIFEXITED(status2)) 
+        exit(WEXITSTATUS(status2));
+    else if (WIFSIGNALED(status2))
+        exit(128 + WTERMSIG(status2));
+	else
+		exit(1);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -189,8 +215,8 @@ int	main(int argc, char **argv, char **envp)
 	if (argc == 5)
 	{
 		pipex(argv, envp);
-		return (EXIT_SUCCESS);
+		return (1);
 	}
 	fprintf(stderr, "Invalid number of arguments: %s\n", argv[0]);
-	return (EXIT_FAILURE);
+	return (1);
 }
