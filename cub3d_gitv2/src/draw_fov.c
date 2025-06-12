5aa2f1bf5b359c28cf7724c3fd8116a07836bbd9
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-06-12 17:00:00 +0200

    added today's changes

--- cub3d_gitv2/src/draw_fov.c
+++ cub3d_gitv2/src/draw_fov.c
@@ -1,16 +1,16 @@
-#include "header.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   draw_fov.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/12 10:18:26 by jilustre      #+#    #+#                 */
+/*   Updated: 2025/06/12 15:48:59 by jilustre      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-// M_PI is defined in <math.h> but squigly line made me define my own.
-// take agle in degree -> return radians
-// double degree_to_radians(double degree)
-// {
-// 	double	pi;
-// 	double	retval;
-	
-// 	pi = PI; 
-// 	retval = degree * pi / 180.0;
-// 	return (retval);
-// }
+#include "header.h"
 
 // Function to set a pixel using mlx_put_pixel
 static void set_pixel(mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color)
@@ -22,50 +22,43 @@ static void set_pixel(mlx_image_t *img, uint32_t x, uint32_t y, uint32_t color)
 	}
 }
 
-/* round off double to int */// -> just use math.h
-// static int double_to_int(double nbr)
-// {
-// 	int res;
-
-// 	if (nbr >= 0)
-// 		res = (int)(nbr + 0.5);
-// 	else
-// 		res = (int)(nbr - 0.5);
-
-// 	return res;
-// }
-
-/* set line struct - startpoint & endpoint. */
-static t_line get_line_endpoints(t_vars *data)
+static t_line ray_wall(t_vars *data, double angle)
 {
 	t_line	line;
+	double	ray_x = data->plx;
+	double	ray_y = data->ply;
+	double	dx = cos(angle);
+	double	dy = sin(angle);
+	double	step = 0.05; // fine enough step for smooth lines
+	int		map_x; 
+	int		map_y;
 
-	// Set starting point at the center
-	// line.x1 = (int)data->plx;
-	// line.y1 = (int)data->ply;.
-	line.x1 = (int)round(data->plx);
-	line.y1 = (int)round(data->ply);
+	line.x1 = (int)(data->plx * 32);
+	line.y1 = (int)(data->ply * 32);
 
-	// Extend line in delta x y dir.
-	line.x2 = line.x1 + (int)(data->pdx * 1);
-	line.y2 = line.y1 + (int)(data->pdy * 1);
-
+	while (1)
+	{
+		ray_x += dx * step;
+		ray_y += dy * step;
+		map_x = (int)(ray_x);
+		map_y = (int)(ray_y);
+		if (data->themap[map_y][map_x] == '1') // hit wall
+			break;
+	}
+	line.x2 = (int)(ray_x * 32);
+	line.y2 = (int)(ray_y * 32);
 	return (line);
 }
 
 // Function to draw a line using Bresenham's algorithm
-void bresenham_line(mlx_image_t *img, t_line line, uint32_t color) {
+void bresenham_line(mlx_image_t *img, t_line line, uint32_t color)
+{
 	int dx = abs(line.x2 - line.x1);
 	int dy = abs(line.y2 - line.y1);
-
 	int sx; // new;
 	int sy;
-
 	int e2;
 	int err;
-
-	// int sx = (line.x1 < line.x2) ? 1 : -1;
-	// int sy = (line.y1 < line.y2) ? 1 : -1; //illegal ternary
 
 	//direction.
 	if (line.x1 < line.x2)
@@ -77,13 +70,12 @@ void bresenham_line(mlx_image_t *img, t_line line, uint32_t color) {
 		sy = 1;
 	else
 		sy = -1;
-
 	err = dx - dy;
-	while (1) {
+	while (1)
+	{
 		set_pixel(img, line.x1, line.y1, color);
 		if (line.x1 == line.x2 && line.y1 == line.y2) // Reached endpoint
 			break;
-
 		e2 = 2 * err;
 		if (e2 > -dy) {
 			err -= dy;
@@ -96,26 +88,23 @@ void bresenham_line(mlx_image_t *img, t_line line, uint32_t color) {
 	}
 }
 
-static void	test_scale_line(t_line *line)
-{
-	line->x1 = line->x1 * 32;
-	line->y1 = line->y1 * 32;
-	line->x2 = line->x2 * 32;
-	line->y2 = line->y2 * 32;
-}
-
 void	draw_fov_line(t_vars *data)
 {
+	const int num_rays = 30;  // number of rays
+	const double fov = PI / 3;  // 60 degrees field of view
+	const double start_angle = data->pla - fov / 2;
+	const double step = fov / num_rays;
+	int	i = 0;
 	t_line line;
 
 	mlx_delete_image(data->mlx, data->fovlines);
-	
 	data->fovlines = mlx_new_image(data->mlx, 700, 400);
-	line = get_line_endpoints(data);
-
-	test_scale_line(&line);
-	
-	bresenham_line(data->fovlines, line, 0xFFFFFF);
-	// printf("test2\n");
+	while (i < num_rays)
+	{
+		double angle = start_angle + i * step;
+		line = ray_wall(data, angle);
+		bresenham_line(data->fovlines, line, 0xFFFFFF);
+		i++;
+	}
 	mlx_image_to_window(data->mlx, data->fovlines, 0, 0);
 }
