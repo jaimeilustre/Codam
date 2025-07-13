ee6de5604572d24e899f1b64ac0c86537c293af9
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-07-13 11:47:19 +0200

    added missing copy and copy assignment decleration and modified attack function

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,29 +6,16 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/26 15:18:23 by jilustre      #+#    #+#                 */
-/*   Updated: 2025/07/01 17:03:41 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/07/13 11:46:09 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
 int main(void)
-{
-	std::cout << "Creating ClapTrap:" << std::endl;
-	ClapTrap	ct("CT-01");
-	ct.attack("Target A");
-	ct.takeDamage(5);
-	ct.beRepaired(3);
-	ct.takeDamage(8);
-	ct.beRepaired(5);
-	ct.attack("Target B");
-	std::cout << std::endl;
-	
-	std::cout << "Creating ScavTrap:" << std::endl;
+{	
 	ScavTrap	st("ST-01");
+	
 	st.attack("Target C");
 	st.takeDamage(50);
 	st.beRepaired(30);
@@ -36,7 +23,6 @@ int main(void)
 	st.beRepaired(50);
 	st.attack("Target D");
 	st.guardGate();
-	std::cout << std::endl;
-	
+
 	return (0);
 }
