1baf03291ead8896d32e286f56a9f9546580f251
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-08-12 15:28:07 -0700

    inital commit ex01; copied files from ex00

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/08/12 10:58:27 by jaimeilustr   #+#    #+#                 */
+/*   Updated: 2025/08/12 14:14:20 by jaimeilustr   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+# include <string>
+
+# define MAX_GRADE 1
+# define MIN_GRADE 150
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat&	operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+		
+		std::string const& getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrementGrade();
+	private:
+		std::string const	_name;
+		int					_grade;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
