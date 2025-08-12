3c9880b3f3cc213466599ebe44594a28531aa28e
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-08-12 12:27:16 -0700

    added exception classes and started creating class functions

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,23 +6,40 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/12 10:58:27 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/08/12 11:41:18 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2025/08/12 12:17:43 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <iostream>
+# include <exception>
 # include <string>
+
+# define MAX_GRADE 1
+# define MIN_GRADE 150
 
 class Bureaucrat
 {
 	public:
-		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const std::string& name, int grade);
 		Bureaucrat(const Bureaucrat& other);
 		Bureaucrat&	operator=(const Bureaucrat& other);
 		~Bureaucrat();
 
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+		
 		std::string const& getName() const;
 		int			const& getGrade() const;
 		void		incrementGrade();
