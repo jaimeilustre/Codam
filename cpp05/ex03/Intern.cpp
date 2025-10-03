c0b6659d8b0dcf31aca2944fe81a082ed59fe4a6
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-10-03 17:59:25 +0200

    started makeForm function

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/09/30 16:04:14 by jilustre      #+#    #+#                 */
-/*   Updated: 2025/09/30 16:07:08 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/10/02 14:06:08 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,9 @@ Intern&	Intern::operator=(const Intern& other) {};
 Intern::~Intern()
 {
 	std::cout << "Intern destructor called!\n" << std::endl;
+}
+
+AForm*	Intern::makeForm(const std::string& formName, const std::string& formTarget)
+{
+	std::cout << "Intern creates " << formName << std::endl;
 }
