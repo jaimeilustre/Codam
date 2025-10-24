c946c94024d240b955fd11053e4d6f07bd8662c0
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-10-24 14:16:48 +0200

    added new forms to Makefile and cleaned up main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/12 10:57:17 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/10/22 15:01:43 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/10/24 14:13:01 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,37 +20,64 @@ int main(void)
 {
 	try
 	{
-		std::cout << "=== BASIC BUREACRAT TESTS ===" << std::endl;
+		std::cout << "=== CREATING BUREAUCRATS ===" << std::endl;
 		
 		Bureaucrat	b1("John Doe", 139);
-		Bureaucrat	b2("Jane Doe", 6);
-		Bureaucrat	b3("Joe Doe", 75);
+		Bureaucrat	b2("Jane Doe", 73);
+		Bureaucrat	b3("Joe Doe", 1);
 		
 		std::cout << b1 << std::endl;
 		std::cout << b2 << std::endl;
 		std::cout << b3 << std::endl;
+		std::cout << std::endl;
 
-		
+		std::cout << "=== CREATING FORMS ===" << std::endl;
+
+		ShrubberyCreationForm	sForm("Form 1");
+		RobotomyRequestForm		rForm("Form 2");
+		PresidentialPardonForm	pForm("Form 3");
+
+		std::cout << sForm << std::endl;
+		std::cout << rForm << std::endl;
+		std::cout << pForm << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "=== SIGNING FORMS ===" << std::endl;
+				
 		try
 		{
-			
+			b1.signForm(sForm);
+			b2.signForm(rForm);
+			b3.signForm(pForm);
 		}
 		catch(std::exception& e)
 		{
 			std::cerr << "Exception caught: " << e.what() << std::endl;
 		}
 
+		std::cout << sForm << std::endl;
+		std::cout << rForm << std::endl;
+		std::cout << pForm << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "=== EXECUTING FORMS ===" << std::endl;		
+
 		try
 		{
-			
+			b1.executeForm(sForm);
+			b2.executeForm(rForm);
+			b3.executeForm(pForm);
 		}
 		catch(std::exception& e)
 		{
 			std::cerr << "Exception caught: " << e.what() << std::endl;
 		}
+		std::cout << std::endl;
+		std::cout << "=== DESTRUCTION ===" << std::endl;
 	}
 	catch(std::exception& e)
 	{
 		std::cerr << "Constructor failed!: " << e.what() << std::endl;
-	}
+	}	
+	return (0);
 }
