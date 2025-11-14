335f6223df6c9a04fe4a64796a3d7d0548887c10
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-11-14 10:51:03 +0100

    final version with modified map

--- exam_practice/exam_05/level_2-c/count_islands.c
+++ exam_practice/exam_05/level_2-c/count_islands.c
@@ -6,17 +6,27 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/11/09 15:10:29 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/11/13 17:45:38 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/11/14 10:50:05 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "count_islands.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-// static void	flood(t_map *map, int y, int x)
+typedef struct s_map
+{
+	int height;
+	int width;
+	char **grid;
+	
+}	t_map;
+
+// static void flood(t_map *map, int y, int x)
 // {
 // 	if (y < 0 || y >= map->height || x < 0 || x >= map->width)
 // 		return ;
-// 	if (map->grid[y][x] != '1')
+// 	if (map->grid[y][x] != 'X')
 // 		return ;
 // 	map->grid[y][x] = 'x';
 // 	flood(map, y + 1, x);
@@ -25,19 +35,19 @@
 // 	flood(map, y, x - 1);
 // }
 
-// static int	count_islands(t_map *map)
-// {
-// 	int	y;
-// 	int	x;
-// 	int	count = 0;
-
+// static int count_islands(t_map *map)
+// {	
+// 	int y;
+// 	int x;
+// 	int count = 0;
+	
 // 	y = -1;
 // 	while (++y < map->height)
 // 	{
 // 		x = -1;
 // 		while (++x < map->width)
 // 		{
-// 			if (map->grid[y][x] == '1')
+// 			if (map->grid[y][x] == 'X')
 // 			{
 // 				count++;
 // 				flood(map, y, x);
@@ -47,12 +57,11 @@
 // 	return (count);
 // }
 
-// return largest island
 static int flood(t_map *map, int y, int x)
 {
 	if (y < 0 || y >= map->height || x < 0 || x >= map->width)
 		return (0);
-	if (map->grid[y][x] != '1')
+	if (map->grid[y][x] != 'X')
 		return (0);
 	map->grid[y][x] = 'x';
 	return (1
@@ -67,101 +76,39 @@ static int count_islands(t_map *map)
 	int y;
 	int x;
 	int max_size = 0;
-	
+
 	y = -1;
 	while (++y < map->height)
 	{
 		x = -1;
 		while (++x < map->width)
 		{
-			if (map->grid[y][x] == '1')
+			if (map->grid[y][x] == 'X')
 			{
 				int size = flood(map, y, x);
 				if (size > max_size)
 					max_size = size;
-			}	
+			}
 		}
 	}
 	return (max_size);
 }
 
-static void	free_map(t_map *map)
+static void free_map(t_map *map)
 {
 	int i = 0;
 	while (i < map->height)
-		free(map->grid[i++]);
+			free(map->grid[i++]);
 	free(map->grid);
 }
 
-// static t_map	*read_map(char *file)
-// {
-// 	int		fd;
-// 	int		len;
-// 	char	buf[10000];
-// 	t_map	*map;
-// 	int		i, j, row, start;
-
-// 	fd = open(file, O_RDONLY);
-// 	if (fd < 0 || (len = read(fd, buf, 9999)) <= 0)
-// 		return (NULL);
-// 	close(fd);
-// 	buf[len] = '\0';
-
-// 	// Step 1: Count lines and width
-// 	map = malloc(sizeof(t_map));
-// 	map->height = 0;
-// 	map->width = 0;
-// 	for (i = 0; i < len; i++)
-// 	{
-// 		if (buf[i] == '\n')
-// 			map->height++;
-// 		else if (map->height == 0)
-// 			map->width++;
-// 	}
-// 	if (buf[len - 1] != '\n') // last line without newline
-// 		map->height++;
-
-// 	// Step 2: Allocate grid
-// 	map->grid = malloc(map->height * sizeof(char *));
-
-// 	// Step 3: Split lines manually 
-// 	row = 0; // current row index in the grid
-// 	start = 0; // marks the beginning of the current line in buf
-// 	for (i = 0; i <= len; i++)
-// 	{
-// 		if (buf[i] == '\n' || buf[i] == '\0')
-// 		{
-// 			// skips empty lines
-// 			if (i == start)
-// 			{
-// 				start = i + 1;
-// 				continue ;
-// 			}
-			
-// 			 // allocate enough space for one full line (+1 for null terminator)
-// 			map->grid[row] = malloc(map->width + 1);
-			
-// 			// copy characters into the grid
-// 			j = 0;
-// 			while (start < i && j < map->width)
-// 				map->grid[row][j++] = buf[start++];
-// 			map->grid[row][j] = '\0';
-
-// 			// move to next row
-// 			row++;
-// 			start = i + 1;
-// 		}
-// 	}
-// 	return (map);
-// }
-
 static t_map	*read_map(char *file)
 {
-	int		fd;
-	int		len;
-	char	buf[10000];
-	t_map	*map;
-	int		i, j, row, start, line_len;
+	int fd;
+	int len;
+	char buf[10000];
+	t_map *map;
+	int i, j, row, start, line_len;
 
 	fd = open(file, O_RDONLY);
 	if (fd < 0 || (len = read(fd, buf, 9999)) <= 0)
@@ -172,8 +119,6 @@ static t_map	*read_map(char *file)
 	map = malloc(sizeof(t_map));
 	map->height = 0;
 	map->width = 0;
-
-	// Step 1: determine width and height
 	for (i = 0; i < len; i++)
 	{
 		if (buf[i] == '\n')
@@ -183,39 +128,37 @@ static t_map	*read_map(char *file)
 	}
 	if (buf[len - 1] != '\n')
 		map->height++;
-
-	// Step 2: allocate grid
+	
 	map->grid = malloc(map->height * sizeof(char *));
 	if (!map->grid)
 		return (free(map), NULL);
 
-	// Step 3: split lines manually and validate
+	for (int k = 0; k < map->height; k++)
+   		map->grid[k] = NULL;
+	
 	row = 0;
 	start = 0;
-	for (i = 0; i <= len; i++)
+	for (int i = 0; i <= len; i++)
 	{
 		if (buf[i] == '\n' || buf[i] == '\0')
 		{
 			line_len = i - start;
-
-			// check for empty line (invalid)
+			
 			if (line_len == 0)
 				return (free_map(map), free(map), NULL);
-
-			// check line length
+			
 			if (line_len != map->width)
 				return (free_map(map), free(map), NULL);
-
+			
 			map->grid[row] = malloc(map->width + 1);
 			if (!map->grid[row])
 				return (free_map(map), free(map), NULL);
-
-			// copy and validate characters
+			
 			j = 0;
 			while (start < i)
 			{
 				char c = buf[start++];
-				if (c != '0' && c != '1')
+				if (c != '.' && c != 'X')
 					return (free_map(map), free(map), NULL);
 				map->grid[row][j++] = c;
 			}
@@ -229,19 +172,16 @@ static t_map	*read_map(char *file)
 	return (map);
 }
 
-
 int main(int argc, char **argv)
 {
+	int i;
+	int n;
+	char buf[12];
 	t_map *map;
-	int		n;
-	char	buf[12];
-	int		i;
 
 	if (argc < 2 || !(map = read_map(argv[1])))
-		return (write(1, "Map Error\n", 10), 1);
+		return (write(1, "Map Error\n", 10),1);
 	n = count_islands(map);
-
-	// essentially itoa/putnbr
 	i = 0;
 	if (n == 0)
 		buf[i++] = '0';
@@ -251,10 +191,10 @@ int main(int argc, char **argv)
 		n /= 10;
 	}
 	while (i--)
-		write (1, &buf[i], 1);
+		write(1, &buf[i], 1);
 	write (1, "\n", 1);
 	free_map(map);
 	free(map);
-	return (0);
+	return (0);		
 }
 
