8e7975fd4e1e2c1781040bc86257e764c09c42d4
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-11-27 15:15:00 +0100

    moved constructors into private and started with print functions

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,18 +6,18 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/30 11:12:21 by jilustre      #+#    #+#                 */
-/*   Updated: 2025/10/30 11:20:24 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/11/27 15:13:12 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter(const std::string& input): _input(input)
+ScalarConverter::ScalarConverter()
 {
 	std::cout << "ScalarConverter parameterized constructor called!" << std::endl;
 }
 
-ScalarConverter::ScalarConverter(const ScalarConverter& other): _input(other._input)
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
 {
 	std::cout << "ScalarConverter copy constructor called!" << std::endl;
 }
@@ -31,4 +31,31 @@ ScalarConverter&	ScalarConverter::operator=(const ScalarConverter& other)
 ScalarConverter::~ScalarConverter()
 {
 	std::cout << "ScalarConverter destructor called!" << std::endl;
+}
+
+// Printing functions
+
+void	ScalarConverter::printChar(char c)
+{
+	std::cout << "char: '" << c << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << static_cast<float>(c) << std::endl;
+	std::cout << "double: " << static_cast<double>(c) << std::endl;
+}
+
+void	ScalarConverter::printInt(int n)
+{
+	if (n >= 32 && n <= 126)
+		std::cout << "char: '" << static_cast<char>(n) << "'" << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+	
+	std::cout << "int: " << n << std::endl;
+	std::cout << "float: " << static_cast<float>(n) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(n) << ".0" << std::endl;
+}
+
+void	ScalarConverter::printFloat(float f)
+{
+	
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/30 10:58:41 by jilustre      #+#    #+#                 */
-/*   Updated: 2025/10/30 11:16:47 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/11/27 14:56:18 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,27 @@
 # define SCALAR_CONVERTER_HPP
 
 # include <iostream>
+# include <cstdlib>
+# include <cctype>
+# include <climits>
+# include <limits>
 
 class ScalarConverter
 {
 	public:
-		ScalarConverter(const std::string& input);
-		ScalarConverter(const ScalarConverter& other);
-		ScalarConverter&	operator=(const ScalarConverter& other);
-		~ScalarConverter();
-
 		static void	convert(const std::string& input);
 		
 	private:
-		const std::string&	_input;
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter&	operator=(const ScalarConverter& other);
+		~ScalarConverter();
 	
+		static void printChar(char c);
+		static void	printInt(int n);
+		static void	printFloat(float f);
+		static void printDouble(double d);
+		
 };
 
 #endif
