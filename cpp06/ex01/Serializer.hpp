8040afa5746b5ecaaa2af9b5fd14d9b52b907e32
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-01 12:08:54 +0100

    initial commit

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Serializer.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/01 12:07:25 by jilustre      #+#    #+#                 */
+/*   Updated: 2025/12/01 12:08:29 by jilustre      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+class Serializer
+{
+	public:
+	private:
+};
+
+#endif
