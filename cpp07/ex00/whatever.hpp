a7db973a70227b93b60ffd6e4b42e0813e68fa90
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-07 10:37:51 +0100

    working but still need to clean up min and max functions

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,13 +6,32 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/07 10:23:18 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/12/07 10:23:36 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2025/12/07 10:34:20 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+# include <iostream>
 
+template <typename T> void swap(T& x, T& y)
+{
+	T	tmp;
+
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <typename T> T min(T x, T y)
+{
+	return (x < y) ? x : y; 
+}
+
+template <typename T> T max(T x, T y)
+{
+	return (x > y) ? x : y;
+}
 
 #endif
