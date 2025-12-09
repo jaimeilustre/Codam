62afc4b9d8c9bfcec5f3c5eb641db95db610933d
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-09 21:47:51 +0100

    initial commit; created class and Makefile

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Array.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/09 21:39:17 by jaimeilustr   #+#    #+#                 */
+/*   Updated: 2025/12/09 21:47:11 by jaimeilustr   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+template <typename T> class Array
+{
+	private:
+		T*				_arr;
+		unsigned int	_size;
+
+	public:
+		// Orthodox canonical form
+		Array();
+		Array(unsigned int size);
+		Array(const Array& other);
+		Array&	operator=(const Array& other);
+		~Array();
+
+		// Element access
+		T&	operator[](unsigned int index);
+		const T&	operator[](unsigned int index) const;
+
+		// Getter
+		unsigned int	getSize() const;
+};
+
+#endif
