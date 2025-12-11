60945daf0ad344758243c92c6b13a207afa72a05
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-11 10:49:43 +0100

    initial commit + wrote template function but still not sure if it works

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   easyfind.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/11 10:28:39 by jilustre      #+#    #+#                 */
+/*   Updated: 2025/12/11 10:49:05 by jilustre      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <vector>
+# include <deque>
+# include <list>
+# include <stdexcept>
+
+template <typename T> typename T::iterator easyfind(T& container, int val)
+{
+	T::iterator it = std::find(container.begin(), container.end(), val);
+
+	if (it == container.end())
+		throw std::runtime_error("Value not found");
+
+	return (it);
+}
+
+#endif
