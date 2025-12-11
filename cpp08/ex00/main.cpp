87cf5867f3b46d21bbb50d904acea3503bb56ad9
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-11 16:15:54 +0100

    addes tests for deque

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/11 15:34:50 by jilustre      #+#    #+#                 */
-/*   Updated: 2025/12/11 15:58:21 by jilustre      ########   odam.nl         */
+/*   Updated: 2025/12/11 16:15:20 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,26 @@ int main(void)
 	{
 		std::cerr << "Vector: " << e.what() << std::endl;
 	}
+
+	std::cout << "=== DEQUE TESTS ===" << std::endl;
+	
+	d.push_front(6);
+	d.push_front(5);
+	d.push_front(4);
+
+	for (int i = 0; i < 3; i++)
+		std::cout << d[i] << " ";
+	std::cout << std::endl;
+
+	try
+	{
+		std::deque<int>::iterator it_d = easyfind(d, 5);
+		std::cout << "Found in deque: " << *it_d << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Deque: " << e.what() << std::endl;
+	}
+	
 	return (0);
 }
