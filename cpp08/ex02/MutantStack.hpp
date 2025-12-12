3b199ba60732b49b8ef8e9342425fd7d184f4ab9
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-12 10:39:01 +0100

    initial commit + created template class that inherits from std::stack

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   MutantStack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/12 10:25:32 by jaimeilustr   #+#    #+#                 */
+/*   Updated: 2025/12/12 10:38:11 by jaimeilustr   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template <typename T> class MutantStack : public std::stack<T>
+{
+	private:
+	public:
+		MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack&	operator=(const MutantStack& other);
+		~MutantStack();
+
+		typedef typename std::stack<T>::container_type::iterator	iterator;
+		typedef typename std::stack<T>::container_type::iterator	reverseIterator;
+		typedef	typename std::stack<T>::container_type::iterator	constIterator;
+		typedef typename std::stack<T>::container_type::iterator	constReverseIterator;
+
+		iterator	begin();
+		iterator	end();
+
+		reverseIterator	rbegin();
+		reverseIterator	rend();
+
+		constIterator	cbegin();
+		constIterator	cend();
+
+		constReverseIterator	crbegin();
+		constReverseIterator	crend();
+};
+
+#endif
