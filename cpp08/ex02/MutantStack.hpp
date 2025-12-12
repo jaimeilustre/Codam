6272cf0b3b97d3c00e5ff861d932e76a20d3169a
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-12 12:14:02 +0100

    fixed issue with wrong typedefs and wrote orthodox canonical form functions in tpp file

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/12 10:25:32 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/12/12 10:39:39 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2025/12/12 12:09:05 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,25 @@
 
 template <typename T> class MutantStack : public std::stack<T>
 {
-	private:
 	public:
 		MutantStack();
 		MutantStack(const MutantStack& other);
 		MutantStack&	operator=(const MutantStack& other);
 		~MutantStack();
 
-		typedef typename std::stack<T>::container_type::iterator	iterator;
-		typedef typename std::stack<T>::container_type::iterator	reverseIterator;
-		typedef	typename std::stack<T>::container_type::iterator	constIterator;
-		typedef typename std::stack<T>::container_type::iterator	constReverseIterator;
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverseIterator;
+		typedef	typename std::stack<T>::container_type::const_iterator			constIterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	constReverseIterator;
 
-		iterator	begin();
-		iterator	end();
+		iterator				begin();
+		iterator				end();
 
-		reverseIterator	rbegin();
-		reverseIterator	rend();
+		reverseIterator			rbegin();
+		reverseIterator			rend();
 
-		constIterator	cbegin();
-		constIterator	cend();
+		constIterator			cbegin();
+		constIterator			cend();
 
 		constReverseIterator	crbegin();
 		constReverseIterator	crend();
--- cpp08/ex02/MutantStack.tpp
+++ cpp08/ex02/MutantStack.tpp
@@ -6,16 +6,26 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/12 10:39:28 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2025/12/12 11:38:10 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2025/12/12 12:08:25 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_TPP
 # define MUTANTSTACK_TPP
 
-template <typename T> MutantStack<T>::MutantStack() : std::stack<T>()
+# include "MutantStack.hpp"
+
+template <typename T> MutantStack<T>::MutantStack() : std::stack<T>() {}
+
+template <typename T> MutantStack<T>::MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+
+template <typename T> MutantStack<T>&	MutantStack<T>::operator=(const MutantStack& other)
 {
-	
+	if (this != &other)
+		std::stack<T>::operator=(other);
+	return (*this);
 }
+
+template <typename T> MutantStack<T>::~MutantStack() {}
 
 #endif
