1ce967b6c5982b12d4b301963631613458052418
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-12-12 15:37:51 +0100

    modified makefile and tested main from subject: everything works

---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jilustre <jilustre@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/12 15:31:20 by jilustre      #+#    #+#                 */
+/*   Updated: 2025/12/12 15:34:36 by jilustre      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+int main(void)
+{
+	MutantStack<int> mstack;
+	
+	mstack.push(5);
+	mstack.push(17);
+	
+	std::cout << mstack.top() << std::endl;
+	
+	mstack.pop();
+	
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
