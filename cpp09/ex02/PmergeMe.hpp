090c53e3dabe000f303a95a5290c5ec6f690867e
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-01-02 09:46:56 +0800

    initial commit + makefile and class created

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   PmergeMe.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/01/02 09:33:30 by jaimeilustr   #+#    #+#                 */
+/*   Updated: 2026/01/02 09:42:23 by jaimeilustr   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <vector>
+# include <deque>
+
+class PmergeMe
+{
+	private:
+		// Parser
+		static int	parserInts(const char* s);
+
+		// Vector container functions
+		static void	fjSort(std::vector<int>& vect);
+		static void	createPairs(const std::vector<int>& input, std::vector<int>& main, std::vector<int>& pend, int& odd);
+		static void	insert(std::vector<int>& main, const std::vector<int>& pend);
+
+		// Deque container functions
+		static void	fjSort(std::deque<int>& deq);
+		static void	createPairs(const std::deque<int>& input, std::deque<int>& main, std::deque<int>& pend, int& odd);
+		static void	insert(std::deque<int>& main, const std::deque<int>& pend);
+		
+	public:
+		static void	sort(int argc, char **argv);
+	
+};
+
+#endif
