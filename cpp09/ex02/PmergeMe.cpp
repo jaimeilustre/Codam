4e7dc533b86720e6fe1e17aca2e2bca569d55dd0
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-01-29 09:03:16 +0100

    added vector sort functions and tested; it works, need to move main to its own file

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/01/02 10:45:45 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2026/01/02 11:29:58 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2026/01/29 09:01:30 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,56 @@ int	PmergeMe::parserInts(const char* s)
 		throw std::runtime_error("Error");
 
 	return static_cast<int>(v);
+}
+
+// Vector version
+void	PmergeMe::createPairs(const std::vector<int>& input, std::vector<int>& main, std::vector<int>& pend, int& odd)
+{
+	size_t	i = 0;
+	for (; i + 1 < input.size(); i += 2)
+	{
+		int a = input[i];
+		int b = input[i + 1];
+		
+		if (a > b)
+			std::swap(a, b);
+		
+		pend.push_back(a);
+		main.push_back(b);
+	}
+	if (i < input.size())
+		odd = input[i];
+}
+
+void	PmergeMe::insert(std::vector<int>& main, const std::vector<int>& pend)
+{
+	for (size_t i = 0; i < pend.size(); ++i)
+	{
+		std::vector<int>::iterator pos = std::lower_bound(main.begin(), main.end(), pend[i]);
+		main.insert(pos, pend[i]);
+	}
+}
+
+void	PmergeMe::fjSort(std::vector<int>& vect)
+{
+	if (vect.size() <= 1)
+		return ;
+	
+	std::vector<int> main;
+	std::vector<int> pend;
+	int odd = -1;
+
+	createPairs(vect, main, pend, odd);
+	fjSort(main);
+	insert(main, pend);
+
+	if (odd != -1)
+	{
+		std::vector<int>::iterator pos = std::lower_bound(main.begin(), main.end(), odd);
+		main.insert(pos, odd);
+	}
+	
+	vect = main;
 }
 
 void	PmergeMe::sort(int argc, char **argv)
@@ -43,7 +93,7 @@ void	PmergeMe::sort(int argc, char **argv)
 
 	// Vector version
 	auto	vectStart = std::chrono::high_resolution_clock::now();
-	
+	fjSort(vect);
 	auto	vectEnd = std::chrono::high_resolution_clock::now();
 
 	// Deque version
@@ -61,4 +111,18 @@ void	PmergeMe::sort(int argc, char **argv)
 
 	std::cout << "Time to process a range of " << vect.size() << " elements with std::vector : " << vectTime << " us" << std::endl;
 	std::cout << "Time to process a range of " << deq.size() << " elements with std::deque : " << deqTime << " us" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		PmergeMe::sort(argc, argv);
+	}
+	catch (...)
+	{
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+	return 0;
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jaimeilustre <jaimeilustre@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/01/02 09:33:30 by jaimeilustr   #+#    #+#                 */
-/*   Updated: 2026/01/02 11:15:40 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2026/01/29 08:58:16 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <deque>
 # include <iostream>
 # include <chrono>
+# include <algorithm>
 
 class PmergeMe
 {
@@ -30,9 +31,9 @@ class PmergeMe
 		static void	insert(std::vector<int>& main, const std::vector<int>& pend);
 
 		// Deque container functions
-		static void	fjSort(std::deque<int>& deq);
-		static void	createPairs(const std::deque<int>& input, std::deque<int>& main, std::deque<int>& pend, int& odd);
-		static void	insert(std::deque<int>& main, const std::deque<int>& pend);
+		// static void	fjSort(std::deque<int>& deq);
+		// static void	createPairs(const std::deque<int>& input, std::deque<int>& main, std::deque<int>& pend, int& odd);
+		// static void	insert(std::deque<int>& main, const std::deque<int>& pend);
 		
 	public:
 		static void	sort(int argc, char **argv);
