e1b263942ea1e99543e83a4ccd34eb8c7b1fb5c0
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-09-02 16:05:40 +0200

    put msg buffer in client struct and rewrote program to accomodate that, seems to be working but still need more testing

--- test/test.c
+++ test/test.c
@@ -11,13 +11,15 @@ typedef struct s_client
 {
 	int				fd;
 	int				id;
+	char			msg[200000];
 	struct s_client	*next;
 
 }	t_client;
 
 t_client	*clients = NULL;
-int			sockfd, g_id;
-char		msg[200000], buf[200000];
+int			sockfd; 
+int			g_id = 0;
+char		buf[200000];
 fd_set		current, read_set, write_set;
 
 // Prints message to standard error
@@ -50,17 +52,17 @@ int maxfd()
 }
 
 // Returns the client's id
-int	getId(int fd)
+t_client	*getClient(int fd)
 {
 	t_client *tmp = clients;
 
 	while (tmp)
 	{
 		if (tmp->fd == fd)
-			return (tmp->id);
+			return (tmp);
 		tmp = tmp->next;
 	}
-	return (-10);
+	return (NULL);
 }
 
 // Sends the global buf string
@@ -83,24 +85,24 @@ void	addClient()
 {
 	t_client			*tmp = clients;
 	t_client			*new;
-	int					client;
 	struct sockaddr_in	clientaddr;
 	socklen_t			len = sizeof(clientaddr);
 
-	client = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
-	if (client == -1)
-		printError();
-
-	bzero(&buf, sizeof(buf));
-	sprintf(buf, "server: client %d just arrived\n", g_id);
-	sendToAll(client);
-	FD_SET(client, &current);
 	new = malloc(sizeof(t_client));
 	if (!new)
 		printError();
-	new->fd = client;
+
+	new->fd = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
+	if (new->fd == -1)
+	{
+		free(new);
+		printError();
+	}
+
 	new->id = g_id++;
+	new->msg[0] = '\0';
 	new->next = NULL;
+
 	if (!clients)
 		clients = new;
 	else
@@ -109,6 +111,10 @@ void	addClient()
 			tmp = tmp->next;
 		tmp->next = new;
 	}
+
+	FD_SET(new->fd, &current);
+	sprintf(buf, "server: client %d just arrived\n", new->id);
+	sendToAll(new->fd);	
 }
 
 void	removeClient(int fd)
@@ -116,9 +122,9 @@ void	removeClient(int fd)
 	t_client	*tmp = clients;
 	t_client	*to_delete = NULL;
 
-	bzero(&buf, sizeof(buf));
-	sprintf(buf, "server: client %d just left\n", getId(fd));
+	sprintf(buf, "server: client %d just left\n", getClient(fd)->id);
 	sendToAll(fd);
+
 	if (clients && clients->fd == fd)
 	{
 		to_delete = clients;
@@ -128,38 +134,47 @@ void	removeClient(int fd)
 	{
 		while (tmp && tmp->next && tmp->next->fd != fd)
 			tmp = tmp->next;
-		if (tmp && tmp->next && tmp->next->fd == fd)
+
+		if (tmp && tmp->next)
 		{
 			to_delete = tmp->next;
 			tmp->next = tmp->next->next;
 		}
 	}
+
 	if (to_delete)
 		free(to_delete);
+
 	FD_CLR(fd, &current);
 	close (fd);
 }
 
-void extract_message(int fd)
+void extract_message(t_client *client)
 {
 	char	tmp[200000];
-	int		i = -1;
-	int		j = -1;
+	char	rest[200000];
+	char	*newline;
+	int		len;
 
-	bzero(&tmp, sizeof(tmp));
-	while (msg[++i] != 0)
+	while ((newline = strstr(client->msg, "\n")))
 	{
-		tmp[++j] = msg[i];
-		if (msg[i] == '\n')
-		{
-			bzero(&buf, sizeof(buf));
-			sprintf(buf, "client %d: %s", getId(fd), tmp);
-			sendToAll(fd);
-			bzero(&tmp, sizeof(tmp));
-			j = -1;
-		}
+		len = newline - client->msg + 1;
+
+		bzero(tmp, sizeof(tmp));
+		bzero(rest, sizeof(rest));
+
+		strcpy(tmp, client->msg);
+		tmp[len] = '\0';
+
+		strcpy(rest, client->msg + len);
+
+		bzero(buf, sizeof(buf));
+		sprintf(buf, "client %d: %s", client->id, tmp);
+		sendToAll(client->fd);
+
+		bzero(client->msg, sizeof(client->msg));
+		strcpy(client->msg, rest);
 	}
-	bzero(&msg, sizeof(msg));
 }
 
 int main(int argc, char **argv)	{
@@ -172,6 +187,9 @@ int main(int argc, char **argv)	{
 
 	struct sockaddr_in servaddr; 
 	bzero(&servaddr, sizeof(servaddr));
+	t_client	*client;
+	int			ret;
+	int			fd;
 
 	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
@@ -180,51 +198,49 @@ int main(int argc, char **argv)	{
 
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
+	if (sockfd == -1)
 		printError();
-	}
 
 	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 		printError();
-	}
-	if (listen(sockfd, 100) != 0) {
+	if (listen(sockfd, 100) != 0)
 		printError();
-	}
 
 	FD_ZERO(&current);
 	FD_SET(sockfd, &current);
-	bzero(&msg, sizeof(msg));
 
 	while (1)
 	{
-		read_set = write_set = current;
+		read_set = current;
+		write_set = current;
+
 		if (select(maxfd() + 1, &read_set, &write_set, NULL, NULL) == -1)
 			continue;
-		for (int fd = 0; fd <= maxfd(); ++fd)
-		{
-			if (FD_ISSET(fd, &read_set))
-			{
-				if (fd == sockfd)
-				{
-					addClient();
-					break;
-				}
-				int ret = 1;
-				while (ret == 1 && msg[strlen(msg) - 1] != '\n')
-				{
-					ret = recv(fd, msg + strlen(msg), 1, 0);
-					if (ret <= 0)
-						break;
-				}
 
-				if (ret <= 0)
-				{
-					removeClient(fd);
-					break;
-				}
-				extract_message(fd);
+		for (fd = 0; fd <= maxfd(); fd++)
+		{
+			if (!FD_ISSET(fd, &read_set))
+				continue;
+
+			if (fd == sockfd)
+			{
+				addClient();
+				break;
 			}
+			
+			client = getClient(fd);
+			
+			ret = recv(fd, client->msg + strlen(client->msg), sizeof(client->msg) - strlen(client->msg) - 1, 0);
+		
+			if (ret <= 0)
+			{
+				removeClient(fd);
+				break;
+			}
+			
+			client->msg[strlen(client->msg) + ret] = '\0';
+			extract_message(client);
 		}
 	}
 	return (0);
